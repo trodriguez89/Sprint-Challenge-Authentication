fe3a47c98415b5e5c634b1c28b8cd9dcc73f3d21
trodriguez89 <trodriguez89@gmail.com>
2020-01-31 10:03:52 -0800

    tests done

--- users/userModel.spec.js
+++ users/userModel.spec.js
@@ -1,8 +1,13 @@
 const db = require("../database/dbConfig");
+const server = require("../api/server");
+const request = require("supertest");
 
 const Users = require("./users-model");
 
 describe("users model", function () {
+  beforeEach(async () => {
+    await db("users").truncate();
+  })
 
   describe("test environment", function () {
     it("should use testing environment", function () {
@@ -10,10 +15,57 @@ describe("users model", function () {
     })
   })
 
-  // describe("add()", function(){
-  //   beforeEach(async () => {
-  //     await db("users").
-  //   })
-  // })
+  //Testing Registering new users
+  describe("/register", function(){
 
+    it("adds new users to db", async function(){
+      await Users.add({ username: "Darth Vader", password: "1234" })
+      await Users.add({ username: "Luke Skywalker", password: "1234" })
+
+      const users = await db("users");
+      expect(users).toHaveLength(2)
+    })
+    it("returns 201 status code when registering", function(){
+      return request(server)
+      .post("/api/auth/register")
+      .send({ username: "Obi-Wan", password: "1234" })
+      .then(response => {
+        expect(response.status).toBe(201)
+      })
+    })
+    it("returns token when registering", function(){
+      return request(server)
+      .post("/api/auth/register")
+      .send({ username: "Darth Vader", password: "1234"})
+      .then(response => {
+        expect(response.body.token)
+      })
+    })
+  })
+
+  //Testing Logging In
+  describe("/login", function(){
+
+    it("returns 200 status code when logging in", function(){
+      return request(server)
+      .post("/api/auth/register")
+      .send({ username: "Darth Vader", password: "1234" })
+      .then(response => {
+        return request(server)
+        .post("/api/auth/login")
+        .send({ username: "Darth Vader", password: "1234"})
+        .then(response => {
+          expect(response.status).toBe(200)
+        })
+      })
+    })
+    it("returns the user ID when logging in", function(){
+      return request(server)
+      .post("/api/auth/login")
+      .send({ username: "Darth Vader", password: "1234"})
+      .then(response => {
+        expect(response.body.id);
+      })
+    })
+  })
 });
