25f42e0d88e20263fa1aa53f5beba50907806342
trodriguez89 <trodriguez89@gmail.com>
2020-01-31 08:09:25 -0800

    initial commit

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+
 
 router.post('/register', (req, res) => {
   // implement registration
